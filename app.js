d7342853d418b79138a7daa81a10a1736e17596c
unknown <theodoreonyejiaku@gmail.com>
2020-08-11 17:12:00 +0100

    change npm start to app.js instead of bin

--- app.js
+++ app.js
@@ -47,4 +47,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+  console.log("Server running on port "+PORT)
+})
