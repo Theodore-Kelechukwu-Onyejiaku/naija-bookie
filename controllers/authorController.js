e440d77cff7bf49946595efd04a4bee5832ebfc0
unknown <theodoreonyejiaku@gmail.com>
2020-09-22 09:34:04 +0100

    implement author detail

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,7 @@
 var Author = require("../models/author");
+var Book = require("../models/book")
+var async = require("async");
+var moment = require("moment")
 
 //Display list of all Authors.
 exports.author_list = function(req, res, next){
@@ -12,8 +15,28 @@ exports.author_list = function(req, res, next){
 };
 
 //Display detail page for a specific Author.
-exports.author_detail = function(req, res){
-    res.send("NOT IMPLEMENTED: Author detail: " +req.parmas.id)
+exports.author_detail = function(req, res, next){
+    async.parallel({
+        author: function(callback){
+            Author.findById(req.params.id)
+                .exec(callback)
+        },
+        author_books: function(callback){
+            Book.find({"author": req.params.id}, "title summary")
+                .exec(callback)
+        }
+    }, (err, result)=>{
+        if(err){
+            return next(err);
+        }
+        if(result.author == null){
+            var err = new Error("Author not found");
+            err.status = 404;
+            return next(err)
+        }
+
+        res.render("author_detail", {title: "Author Detail",author: result.author,author_books: result.author_books, date_born: moment(result.author.date_of_birth).format("YYYY"), date_dead: moment(result.author.date_of_death).format("YYYY")})
+    })
 }
 
 //Display Author create form on GET.
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 
 1)virtuals: a field added to a document in a collection but not real
 2)async_parallel
-3)Book.find({}, "title author") : returns the Book Object with only "title" and "author" fields.
+3)Book.find({}, "title author") : returns the Book Object with only "title" and "author" fields. This is projection in mongoose!!! Wowww
 4).populate("author"): returns the details of this author field instead of just its id, since it is a document inside the Book Collection.
 5).execute(): this is used after the .populate() method
 6)book_list.sort(): this is an array that will sort the list of books alphabetically
